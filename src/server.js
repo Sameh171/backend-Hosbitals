d49101c7d636680b1948782a5e0d2a210191cb81
Sameh171 <samehhussein484@gmail.com>
2025-11-06 17:12:22 +0200

    2

--- src/server.js
+++ src/server.js
@@ -22,7 +22,7 @@ app.use(helmet());
 
 // CORS configuration
 app.use(cors({
-  origin: process.env.CORS_ORIGIN || 'http://localhost:5173',
+  origin: process.env.CORS_ORIGIN || '*',
   credentials: true
 }));
 
